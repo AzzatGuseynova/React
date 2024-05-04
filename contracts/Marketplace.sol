1fc91c02a8e95baa3105214947558609e7eb9358
Denis Zemskikh <zemskikhdenis@gmail.com>
2024-05-04 14:34:35 +0300

    hot fix for forwardsprotocol

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,15 +1,136 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+contract ERC721 {
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
 
-/**
- * @title Marketplace for ERC721 tokens
- * @dev Implements buying and renting of NFTs with ERC721 standard. Includes admin controls and basic financial transactions.
- * @custom:dev-run-script /scripts/deploy_with_market.ts
- */
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => address) private _tokenApprovals;
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function balanceOf(address owner) public view returns (uint256) {
+        require(owner != address(0), "ERC721: balance query for the zero address");
+        return _balances[owner];
+    }
+
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address owner = _owners[tokenId];
+        require(owner != address(0), "ERC721: owner query for nonexistent token");
+        return owner;
+    }
+
+    function approve(address to, uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        require(to != owner, "ERC721: approval to current owner");
+        require(msg.sender == owner || isApprovedForAll(owner, msg.sender), "ERC721: approve caller is not owner nor approved for all");
+        
+        _approve(to, tokenId);
+    }
+
+    function _approve(address to, uint256 tokenId) internal {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(ownerOf(tokenId), to, tokenId);
+    }
+
+    function isApprovedForAll(address owner, address operator) public view returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+    function setApprovalForAll(address operator, bool approved) public {
+        require(operator != msg.sender, "ERC721: approve to caller");
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    function _mint(address to, uint256 tokenId) internal {
+        require(to != address(0), "ERC721: mint to the zero address");
+        require(_owners[tokenId] == address(0), "ERC721: token already minted");
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    function _transfer(address from, address to, uint256 tokenId) internal {
+        require(ownerOf(tokenId) == from, "ERC721: transfer of token that is not own");
+        require(to != address(0), "ERC721: transfer to the zero address");
+
+        _approve(address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+}
+
+
+contract AccessControl {
+    mapping(bytes32 => mapping(address => bool)) private _roles;
+
+    event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender);
+    event RoleRevoked(bytes32 indexed role, address indexed account, address indexed sender);
+
+    modifier onlyRole(bytes32 role) {
+        require(_roles[role][msg.sender], "AccessControl: sender does not have role");
+        _;
+    }
+
+    function grantRole(bytes32 role, address account) public onlyRole(role) {
+        _grantRole(role, account);
+    }
+
+    function revokeRole(bytes32 role, address account) public onlyRole(role) {
+        _revokeRole(role, account);
+    }
+
+    function _grantRole(bytes32 role, address account) internal {
+        if (!_roles[role][account]) {
+            _roles[role][account] = true;
+            emit RoleGranted(role, account, msg.sender);
+        }
+    }
+
+    function _revokeRole(bytes32 role, address account) internal {
+        if (_roles[role][account]) {
+            _roles[role][account] = false;
+            emit RoleRevoked(role, account, msg.sender);
+        }
+    }
+}
+
+contract ReentrancyGuard {
+    bool private _reentrantLock = false;
+
+    modifier nonReentrant() {
+        require(!_reentrantLock, "ReentrancyGuard: reentrant call");
+        _reentrantLock = true;
+        _;
+        _reentrantLock = false;
+    }
+}
+
 contract Marketplace is ERC721, AccessControl, ReentrancyGuard {
     struct Product {
         address owner;
@@ -37,69 +158,23 @@ contract Marketplace is ERC721, AccessControl, ReentrancyGuard {
     event ProductSold(uint256 indexed productId, address indexed buyer, address indexed seller, uint256 price);
     event ProductRented(uint256 indexed productId, address indexed renter, address indexed owner, uint256 price, uint256 expirationTime);
 
-    modifier onlyAdmin() {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
-        _;
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        _grantRole(ADMIN_ROLE, msg.sender);
     }
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 _referralBonus,
-        uint256 _minSalePrice,
-        uint256 _minRentPrice,
-        uint256 _feePercentage,
-        uint256 _defaultExpirationTime
-    ) ERC721(name, symbol) {
-        referralBonus = _referralBonus;
-        minSalePrice = _minSalePrice;
-        minRentPrice = _minRentPrice;
-        feePercentage = _feePercentage;
-        defaultExpirationTime = _defaultExpirationTime;
-    }
-
-
-    /**
-     * @dev Overridden to support multiple interface formats.
-     * @param interfaceId ID of the interface to check.
-     * @return bool representing whether the interface is supported.
-     */
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
-        return super.supportsInterface(interfaceId);
-    }
-
-    
-
-    /// @notice Adds a new product to the marketplace
-    /// @dev Mints a new NFT upon adding a product
-    /// @param _name Name of the product
-    /// @param _price Price of the product in wei
-    /// @param _isForSale Whether the product is for sale
-    /// @param _isForRent Whether the product is for rent
-    /// @param _expirationTime Duration in seconds the product is available for rent after being rented
-    function addProduct(
-        string memory _name,
-        uint256 _price,
-        bool _isForSale,
-        bool _isForRent,
-        uint256 _expirationTime
-    ) external onlyAdmin {
-        require(_isForSale || _isForRent, "Product must be for sale or rent.");
-        require(_price >= (_isForSale ? minSalePrice : minRentPrice), "Price too low.");
-
-        uint256 newProductId = products.length;
+    function addProduct(string memory name, uint256 price, bool isForSale, bool isForRent, uint256 expirationTime) public onlyRole(ADMIN_ROLE) {
         products.push(Product({
             owner: msg.sender,
-            name: _name,
-            price: _price,
-            isForSale: _isForSale,
-            isForRent: _isForRent,
+            name: name,
+            price: price,
+            isForSale: isForSale,
+            isForRent: isForRent,
             renter: address(0),
-            expirationTime: block.timestamp + (_expirationTime == 0 ? defaultExpirationTime : _expirationTime)
+            expirationTime: block.timestamp + expirationTime
         }));
-        _mint(msg.sender, newProductId);
-
-        emit ProductAdded(newProductId, msg.sender, _name, _price, _isForSale, _isForRent);
+        uint256 productId = products.length - 1;
+        _mint(msg.sender, productId);
+        emit ProductAdded(productId, msg.sender, name, price, isForSale, isForRent);
     }
 
     /**
@@ -123,6 +198,7 @@ contract Marketplace is ERC721, AccessControl, ReentrancyGuard {
 
         emit ProductSold(_productId, msg.sender, product.owner, product.price);
     }
+
 
     /**
      * @notice Rents a product, assigning temporary possession to the renter
@@ -162,23 +238,23 @@ contract Marketplace is ERC721, AccessControl, ReentrancyGuard {
         }
     }
 
-    function updateReferralBonus(uint256 _newReferralBonus) external onlyAdmin {
-        referralBonus = _newReferralBonus;
-    }
+    function updateReferralBonus(uint256 _newReferralBonus) external onlyRole(ADMIN_ROLE) {
+    referralBonus = _newReferralBonus;
+    } 
 
-    function updateMinSalePrice(uint256 _newMinSalePrice) external onlyAdmin {
+    function updateMinSalePrice(uint256 _newMinSalePrice) external onlyRole(ADMIN_ROLE) {
         minSalePrice = _newMinSalePrice;
     }
 
-    function updateMinRentPrice(uint256 _newMinRentPrice) external onlyAdmin {
+    function updateMinRentPrice(uint256 _newMinRentPrice) external onlyRole(ADMIN_ROLE) {
         minRentPrice = _newMinRentPrice;
     }
 
-    function updateFeePercentage(uint256 _newFeePercentage) external onlyAdmin {
+    function updateFeePercentage(uint256 _newFeePercentage) external onlyRole(ADMIN_ROLE) {
         feePercentage = _newFeePercentage;
     }
 
-    function withdrawFunds() external onlyAdmin {
+    function withdrawFunds() external onlyRole(ADMIN_ROLE) {
         uint256 balance = address(this).balance;
         payable(msg.sender).transfer(balance);
     }
